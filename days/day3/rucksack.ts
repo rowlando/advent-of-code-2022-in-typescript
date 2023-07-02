b28f84e6677a3b34f09b3345fcd92c1e7d03d977
Nick Rowlands <89347+rowlando@users.noreply.github.com>
2023-07-02 12:22:31 +0100

    Sum all rucksack priorities

--- days/day3/rucksack.ts
+++ days/day3/rucksack.ts
@@ -1,12 +1,18 @@
+import fs from 'fs';
 import { convertLetterToNumberWhereLowercaseAIsOneAndUppercaseZis52 } from "./convertLetterToNumberWhereLowercaseAIsOneAndUppercaseZis52";
 import { findLetterWhichAppearsInBothCompartments } from "./findLetterWhichAppearsInBothCompartments";
 import { separateRucksackInHalf } from "./separateRucksackInHalf";
-const rucksack = "vJrwpWtwJgWrhcsFMMfFFhFp";
 
-console.log(separateRucksackInHalf(rucksack));
-
-const letter = findLetterWhichAppearsInBothCompartments(separateRucksackInHalf(rucksack));
-console.log(letter);
-
-const number = convertLetterToNumberWhereLowercaseAIsOneAndUppercaseZis52(letter);
-console.log(number);
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    let sumOfPriorities = 0;
+    data.split('\n').forEach((rucksack: string) => {
+        const compartments = separateRucksackInHalf(rucksack);
+        const item = findLetterWhichAppearsInBothCompartments(compartments);
+        const priority = convertLetterToNumberWhereLowercaseAIsOneAndUppercaseZis52(item);
+        if (priority) {
+            sumOfPriorities += priority;
+        }
+    });
+    console.log(sumOfPriorities);
+});
